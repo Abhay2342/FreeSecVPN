76ebe9430077a166f8189636d6490d904b5fea7d
Abhay2342 <choudharyabhay2342@gmail.com>
2024-03-16 15:35:23 +0530

    Added Login, SignUp Routes
    Added Dabase and Environment Variables Implementation

---
+++ BackEnd/controllers/login.mjs
@@ -0,0 +1,27 @@
+import db from "../db/conn.mjs";
+import bcrypt from 'bcrypt';
+
+const login = async (req, res) => {
+    const { email, password } = req.body;
+
+    const collection = db.collection("users");
+    try {
+        const user = await collection.findOne({ email });
+        if (user) {
+            const passwordMatch = await bcrypt.compare(password, user.password);
+
+            if (passwordMatch) {
+                res.status(200).send([user]);
+            } else {
+                res.status(401).send("Invalid Credentials!");
+            }
+        } else {
+            res.status(401).send("Invalid Credentials!");
+        }
+    } catch (error) {
+        console.error("Error during login:", error);
+        res.status(500).send("Internal Server Error");
+    }
+};
+
+export default login;
--- BackEnd/server.mjs
+++ BackEnd/server.mjs
@@ -1,12 +1,27 @@
 import express from "express";
+import "./loadEnvironment.mjs";
+import cors from 'cors';
+import login from './controllers/login.mjs';
+import signup from "./controllers/signup.mjs";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Welcome to my server!');
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Running 3000");
+});
+
+app.post("/login", (req, res) => {
+    login(req, res);
+});
+
+app.put("/signup", (req, res) => {
+    signup(req, res);
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+    console.log(`app is on port ${port}`);
 });
